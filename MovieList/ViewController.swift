9098089129a29cc7b3cb4d415678f5d67e0b60a5
Amit Saxena <aamitsaxenaa1@gmail.com>
2020-06-10 21:45:02 +0530

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Amit Saxena on 10/06/20.
+//  Copyright © 2020 Amit Saxena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
